beec57154a67aa2cc9d4560fc53f5ce8c83cd4df
Vitalia <vitaliya.m05@gmail.com>
2025-05-07 11:50:25 +0200

    Next 4x tasks are done

---
+++ src/task-5.ts
@@ -0,0 +1,16 @@
+function logStatus(status: "loading" | "success" | "error"): void {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+logStatus("loading");
+
+// Типізуй параметр status так, щоб дозволити тільки три конкретні рядкові значення: "loading", "success", "error"
+// Переконайся, що TypeScript не дозволяє передати будь-яке інше значення.
+// Типізуй повернення функції, зауваж що вона нічого явно не повертає.
+// Залиш реалізацію функції без змін.
---
+++ src/task-8.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+async function fetchPosts() {
+  const response = await axios.get<Post[]>(
+    "<https://jsonplaceholder.typicode.com/posts>"
+  );
+  return response.data;
+}
+
+fetchPosts().then((posts) => {
+  console.log(posts[0].title);
+});
+
+// Інсталюй бібліотеку axios командою npm i axios
+// Створи інтерфейс Post, який описує об'єкт поста з такими полями:
+// id: число
+// title: рядок
+// body: рядок
+// Типізуй axios.get, щоб вказати, що API повертає масив постів.
